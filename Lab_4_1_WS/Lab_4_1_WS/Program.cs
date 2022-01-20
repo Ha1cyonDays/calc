79501dbb67fbc499d18be3f860d68d412aa3adfe
Valentine <babeinthebar@mail.ru>
2022-01-20 22:04:56 +0700

    ввести б

--- Lab_4_1_WS/Lab_4_1_WS/Program.cs
+++ Lab_4_1_WS/Lab_4_1_WS/Program.cs
@@ -6,8 +6,16 @@ namespace Lab_4_1_WS
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            int num1 = 0;
+
+            // Ask the user to type the first number.
+            Console.WriteLine("Type a number, and then press Enter");
+            num1 = Convert.ToInt32(Console.ReadLine());
+
+            int num2 = 0;
+            // Ask the user to type the second number.
+            Console.WriteLine("Type another number, and then press Enter");
+            num2 = Convert.ToInt32(Console.ReadLine());
         }
     }
 }
