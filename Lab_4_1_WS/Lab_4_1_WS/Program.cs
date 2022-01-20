8f491ee31bee804918e3e064ecc41850c48e9c29
Valentine <babeinthebar@mail.ru>
2022-01-20 22:47:45 +0700

    плюс

--- Lab_4_1_WS/Lab_4_1_WS/Program.cs
+++ Lab_4_1_WS/Lab_4_1_WS/Program.cs
@@ -6,6 +6,24 @@ namespace Lab_4_1_WS
     {
         static void Main(string[] args)
         {
+            int num1 = 0;
+
+            // Ask the user to type the first number.
+            Console.WriteLine("Type a number, and then press Enter");
+            num1 = Convert.ToInt32(Console.ReadLine());
+
+            int num2 = 0;
+            // Ask the user to type the second number.
+            Console.WriteLine("Type another number, and then press Enter");
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Choose an option from the following list:");
+            Console.WriteLine("\ta - Add");
+            Console.WriteLine("\ts - Subtract");
+            Console.WriteLine("\tm - Multiply");
+            Console.WriteLine("\td - Divide");
+            Console.Write("Your option? "); 
+
             switch (Console.ReadLine())
             {
                 case "a":
