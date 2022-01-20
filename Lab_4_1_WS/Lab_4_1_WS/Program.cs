b7b7327c867f8c8a8196d693796c86093d37f829
Valentine <babeinthebar@mail.ru>
2022-01-20 21:41:20 +0700

    фигня какая то

---
+++ Lab_4_1_WS/Lab_4_1_WS/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Lab_4_1_WS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
